986950c2dada393d918fd52fa9d7248e7503223e
Backbak100 <miko.narayan@gmail.com>
2026-08-13 04:14:54 +0530

    added a function
    so that the subscription to the gpu stats channel is not "re found" on every iteration of the while loop in main
    
    made it so that the 16 states in the subscribed channel is not hardcoded

--- livestats.c
+++ livestats.c
@@ -1,64 +1,65 @@
 #include "IOReport.h"
 #include <unistd.h>
 #include <time.h>
+#include "livestats.h"
 
-//gets the gpu's active residency as a float
-//subscription should be a parameter- not constantly reestablished (ex. cluttered in a forever while loop)
-float get_gpu_residency_percent(void) {
-
-    __block long value, value2, off, off2;
+IOReportSubscriptionRef get_input(CFMutableDictionaryRef *subbedChannels) {
     CFMutableDictionaryRef channels = IOReportCopyChannelsInGroup(CFSTR("GPU Stats"), CFSTR("GPU Performance States"), 0, 0, 0);
 
-        CFMutableDictionaryRef subbedChannels = NULL;
-        IOReportSubscriptionRef subscription = IOReportCreateSubscription(NULL, channels, &subbedChannels, 0, 0);
-        while (1){
-            IOReportSampleRef samples = IOReportCreateSamples(subscription, subbedChannels, NULL);
-            sleep(1);
-            IOReportSampleRef samples2 = IOReportCreateSamples(subscription, subbedChannels, NULL);
+    IOReportSubscriptionRef subscription = IOReportCreateSubscription(NULL, channels, subbedChannels, 0, 0);
 
-            IOReportIterate(samples,
-                ^int(IOReportChannelRef channel) {
-                    int format = IOReportChannelGetFormat(channel);
+    CFRelease(channels);
 
-                    value = 0;
+    return subscription;
+}
 
-                    if (format == kIOReportFormatState) {
-                        off = IOReportStateGetResidency(channel, 0);
-                        for (int i = 0; i <16; i++)
-                            value += IOReportStateGetResidency(channel, i);
-                    }
+float get_gpu_residency_percent(IOReportSubscriptionRef subscription, CFMutableDictionaryRef subbedChannels) {
 
-                    return kIOReportIterOk; 
-                }
-            );
+    __block long value, value2, off, off2;
 
-            IOReportIterate(samples2,
-                ^int(IOReportChannelRef channel) {
-                    int format = IOReportChannelGetFormat(channel);
+    IOReportSampleRef samples = IOReportCreateSamples(subscription, subbedChannels, NULL);
+    sleep(1);
+    IOReportSampleRef samples2 = IOReportCreateSamples(subscription, subbedChannels, NULL);
 
-                    value2=0;
+    IOReportIterate(samples,
+        ^int(IOReportChannelRef channel) {
+            int format = IOReportChannelGetFormat(channel);
 
-                    if (format == kIOReportFormatState) {
-                        off2 = IOReportStateGetResidency(channel, 0);
-                        for (int i = 0; i <16; i++)
-                            value2 += IOReportStateGetResidency(channel, i);
-                    }
+            value = 0;
 
-                    return kIOReportIterOk; 
-                }
-            );
-            
-            long doff = off2-off;
-            long dval = value2-value;
-            float percentage = ((float)doff/(float)dval) *100.0f;
-            percentage = 100.0f-percentage;
-        
-            CFRelease(samples);
-            CFRelease(samples2);
-            CFRelease(channels);
-            CFRelease(subbedChannels);
-            return percentage;
+            if (format == kIOReportFormatState) {
+                off = IOReportStateGetResidency(channel, 0);
+                for (int i = 0; i < IOReportStateGetCount(channel); i++)
+                    value += IOReportStateGetResidency(channel, i);
+            }
+
+            return kIOReportIterOk; 
         }
+    );
 
+    IOReportIterate(samples2,
+        ^int(IOReportChannelRef channel) {
+            int format = IOReportChannelGetFormat(channel);
+
+            value2=0;
+
+            if (format == kIOReportFormatState) {
+                off2 = IOReportStateGetResidency(channel, 0);
+                for (int i = 0; i < IOReportStateGetCount(channel); i++)
+                    value2 += IOReportStateGetResidency(channel, i);
+            }
+
+            return kIOReportIterOk; 
+        }
+    );
+            
+    long doff = off2-off;
+    long dval = value2-value;
+    float percentage = ((float)doff/(float)dval) *100.0f;
+    percentage = 100.0f-percentage;
+        
+    CFRelease(samples);
+    CFRelease(samples2);
+    return percentage;
 
 }
