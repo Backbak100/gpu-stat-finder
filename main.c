1c498e68aa963180fab1be5ee69a75c799e1c366
Backbak100 <miko.narayan@gmail.com>
2026-08-13 04:13:16 +0530

    updated gpu info fetcher
    made it so that the subscription to gpu stats channel is not "re-found" on every iteration of the while loop, and changed staticstats and livestats to header files to match new updates

--- main.c
+++ main.c
@@ -1,25 +1,32 @@
-#include "staticstats.c"
-#include "livestats.c"
+#include "staticstats.h"
+#include "livestats.h"
 
 //compile with -framework CoreFoundation -framework IOKit -lIOReport -fblocks
 
 int applesilicon_mchip = 1;
 
 int main (void) {
-
     if (applesilicon_mchip) {
-        struct timespec start, end;
-        gpu_info_t *gpu_info = get_gpu_info();
-        
-        while(gpu_info) {
-            gpu_info_t *next = gpu_info->next_gpu;
-            printf("GPU %d\nGPU core count: %d\nChip model: %s\n\n", gpu_info->gpu_num, gpu_info->core_count, gpu_info->chip_model);
-            free(gpu_info);
-            gpu_info = next;
-        }
-
-        while (1){
-            printf("\r\033[KGPU Active Residency = %.2f%%", get_gpu_residency_percent()); fflush(stdout);}
+            struct timespec start, end;
+            gpu_info_t *gpu_info = get_gpu_info();
+            
+            while(gpu_info) {
+                gpu_info_t *next = gpu_info->next_gpu;
+                printf("GPU %d\nGPU core count: %d\nChip model: %s\n\n", gpu_info->gpu_num, gpu_info->core_count, gpu_info->chip_model);
+                free(gpu_info);
+                gpu_info = next;
+            }
+    
+            CFMutableDictionaryRef subbedChannels = NULL;
+            IOReportSubscriptionRef subscription = get_input(&subbedChannels);
+    
+            while (1) {
+                printf("\r\033[KGPU Active Residency = %.2f%%", get_gpu_residency_percent(subscription, subbedChannels));
+                fflush(stdout);
+            }
+    
+            CFRelease(subscription);
+            CFRelease(subbedChannels);
     }
 
     return 0;
