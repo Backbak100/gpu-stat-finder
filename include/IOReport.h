04e3c1247ce147d6a3d33efb3156ab03f2812c0a
Backbak100 <miko.narayan@gmail.com>
2026-08-16 22:25:45 +0530

    added MCFRelease()

--- include/IOReport.h
+++ include/IOReport.h
@@ -2,7 +2,7 @@
 #define IOReport_H
 
 #include <stdint.h>
-#include <CoreFoundation/CoreFoundation.h>
+#include "CoreFoundation.h"
 #include <DriverKit/IOReportTypes.h>
 
 /*
@@ -27,11 +27,11 @@ extract those values
 typedef struct IOReportSubscriptionRef *IOReportSubscriptionRef;
 
 //IOReportChannels are represented as CFDictionaries
-//destroy with CFRelease()
+//destroy with MCFRelease()
 typedef CFDictionaryRef IOReportChannelRef;
 
 //IOReportSamples are represented as CFDictionaries
-//destroy with CFRelease()
+//destroy with MCFRelease()
 typedef CFDictionaryRef IOReportSampleRef;
 
 //An 'Ok' for the iteration function? in ioreport2.h
@@ -45,16 +45,16 @@ enum {
 //every channel is given a channel_id, a subgroup, and a group
 //a channel is a data stream exposed by a driver or IOKit
 //flags can all be put as 0 for default execution- idk what other flags exist
-//destroy its return value with CFRelease()
+//destroy its return value with MCFRelease()
 extern CFMutableDictionaryRef IOReportCopyChannelsInGroup(
     CFStringRef group, CFStringRef subgroup, uint64_t flags1, uint64_t flags2, uint64_t flags3);
 
 //gets the channel name and returns it as a CFString
-//do not need to destroy its return value with CFRelease()
+//do not need to destroy its return value with MCFRelease()
 extern CFStringRef IOReportChannelGetChannelName(IOReportChannelRef channel);
 
 //gets the channel unit label and returns it as a CFString
-//do not need to destroy its return value with CFRelease()
+//do not need to destroy its return value with MCFRelease()
 extern CFStringRef IOReportChannelGetUnitLabel(IOReportChannelRef channel);
 
 /*gets the channel format and returns it as:
@@ -70,7 +70,7 @@ extern int IOReportChannelGetFormat(CFDictionaryRef samples);
 
 //A subscription allows you to easily poll an interesting channel
 //(for a use NULL, and b use 0 or NULL, im not sure what their acc purpose is)
-//destroy its return value with CFRelease()
+//destroy its return value with MCFRelease()
 extern IOReportSubscriptionRef IOReportCreateSubscription(
     void* a, CFMutableDictionaryRef desiredChannels, CFMutableDictionaryRef* subbedChannels, uint64_t channel_id, CFTypeRef b);
 
@@ -78,7 +78,7 @@ extern IOReportSubscriptionRef IOReportCreateSubscription(
 
 //returns a CFDictionary with the data in the subscribed channels at that point in time
 //Put a as NULL
-//destroy its return value with CFRelease()
+//destroy its return value with MCFRelease()
 extern CFDictionaryRef IOReportCreateSamples(IOReportSubscriptionRef iorsub, CFMutableDictionaryRef subbedChannels, CFTypeRef a);
 
 /*ITERATOR*/
@@ -100,7 +100,7 @@ extern long IOReportSimpleGetIntegerValue(IOReportChannelRef channel, int a);
 //gets residency from a kIOReportFormatState ??
 extern uint64_t IOReportStateGetResidency(CFDictionaryRef channel, int a);
 
-//do not need to destroy its return value with CFRelease()
+//do not need to destroy its return value with MCFRelease()
 extern CFStringRef IOReportStateGetNameForIndex(CFDictionaryRef channel, int state);
 
 //gets num of states in a channel as int
